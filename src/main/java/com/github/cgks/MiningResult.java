258fc513cac9c9099d85ce6339c091a6ec8568da
mathusanm6 <hi@mathusan.me>
2025-05-16 13:00:34 +0200

    fix: each itemset has a different frequency

--- src/main/java/com/github/cgks/MiningResult.java
+++ src/main/java/com/github/cgks/MiningResult.java
@@ -3,19 +3,19 @@ package com.github.cgks;
 import java.util.List;
 
 public class MiningResult {
-    private List<List<Integer>> pattern; // une liste de motifs, ex: [[1, 4], [7, 8]]
+    private List<Integer> pattern; // une liste de motifs, ex: [1, 4]
     private int freq; // fréquence du motif
 
-    public MiningResult(List<List<Integer>> pattern, int freq) {
+    public MiningResult(List<Integer> pattern, int freq) {
         this.pattern = pattern;
         this.freq = freq;
     }
 
-    public List<List<Integer>> getPattern() {
+    public List<Integer> getPattern() {
         return pattern;
     }
 
-    public void setPattern(List<List<Integer>> pattern) {
+    public void setPattern(List<Integer> pattern) {
         this.pattern = pattern;
     }
 
