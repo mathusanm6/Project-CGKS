34a8641d38ad6f7f1507df2e1a123fd3ce5994e0
mathusanm6 <hi@mathusan.me>
2025-05-16 16:09:22 +0200

    feat: add all query types to Miner interface

--- src/main/java/com/github/cgks/Miner.java
+++ src/main/java/com/github/cgks/Miner.java
@@ -9,5 +9,17 @@ public interface Miner {
 
     List<MiningResult> extractClosed(String datasetPath, Map<String, String> params);
 
-    // TODO: add other methods for other mining tasks
+    List<MiningResult> extractMaximal(String datasetPath, Map<String, String> params);
+
+    List<MiningResult> extractRare(String datasetPath, Map<String, String> params);
+
+    List<MiningResult> extractGenerators(String datasetPath, Map<String, String> params);
+
+    List<MiningResult> extractMinimal(String datasetPath, Map<String, String> params);
+
+    List<MiningResult> extractSizeBetween(String datasetPath, Map<String, String> params);
+
+    List<MiningResult> extractPresence(String datasetPath, Map<String, String> params);
+
+    List<MiningResult> extractAbsence(String datasetPath, Map<String, String> params);
 }
