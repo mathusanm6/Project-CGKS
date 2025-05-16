a75d6b990b53a7d5eb5240e9297056ae1fbedd43
mathusanm6 <hi@mathusan.me>
2025-05-16 16:46:51 +0200

    fix: Miner interface functions throws Exception

--- src/main/java/com/github/cgks/Miner.java
+++ src/main/java/com/github/cgks/Miner.java
@@ -5,21 +5,21 @@ import java.util.Map;
 
 public interface Miner {
 
-    List<MiningResult> extractFrequent(String datasetPath, Map<String, String> params);
+    List<MiningResult> extractFrequent(String datasetPath, Map<String, String> params) throws Exception;
 
-    List<MiningResult> extractClosed(String datasetPath, Map<String, String> params);
+    List<MiningResult> extractClosed(String datasetPath, Map<String, String> params) throws Exception;
 
-    List<MiningResult> extractMaximal(String datasetPath, Map<String, String> params);
+    List<MiningResult> extractMaximal(String datasetPath, Map<String, String> params) throws Exception;
 
-    List<MiningResult> extractRare(String datasetPath, Map<String, String> params);
+    List<MiningResult> extractRare(String datasetPath, Map<String, String> params) throws Exception;
 
-    List<MiningResult> extractGenerators(String datasetPath, Map<String, String> params);
+    List<MiningResult> extractGenerators(String datasetPath, Map<String, String> params) throws Exception;
 
-    List<MiningResult> extractMinimal(String datasetPath, Map<String, String> params);
+    List<MiningResult> extractMinimal(String datasetPath, Map<String, String> params) throws Exception;
 
-    List<MiningResult> extractSizeBetween(String datasetPath, Map<String, String> params);
+    List<MiningResult> extractSizeBetween(String datasetPath, Map<String, String> params) throws Exception;
 
-    List<MiningResult> extractPresence(String datasetPath, Map<String, String> params);
+    List<MiningResult> extractPresence(String datasetPath, Map<String, String> params) throws Exception;
 
-    List<MiningResult> extractAbsence(String datasetPath, Map<String, String> params);
+    List<MiningResult> extractAbsence(String datasetPath, Map<String, String> params) throws Exception;
 }
