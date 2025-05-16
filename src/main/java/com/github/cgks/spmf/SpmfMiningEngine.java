27ddbb46295073aacce86dbe94d694b954e28e3c
mathusanm6 <hi@mathusan.me>
2025-05-16 13:21:01 +0200

    feat: add SPMFminer/engine

---
+++ src/main/java/com/github/cgks/spmf/SpmfMiningEngine.java
@@ -0,0 +1,30 @@
+package com.github.cgks.spmf;
+
+import java.util.List;
+import java.util.Map;
+
+import com.github.cgks.MiningRequest;
+import com.github.cgks.MiningResult;
+
+public class SpmfMiningEngine {
+    @Override
+    public List<MiningResult> runMining(MiningRequest request) {
+        // TODO(unknown): Implement the logic to run the mining algorithm using SPMF.
+
+        String queryType = request.getQueryType();
+        String datasetPath = "chemin/vers/spmf/" + request.getDataset();
+        Map<String, String> params = request.getParams();
+
+        switch (queryType) {
+            case "frequent":
+                return SpmfMiner.extractFrequent(datasetPath, params);
+            case "closed":
+                return SpmfMiner.extractClosed(datasetPath, params);
+            case "maximal":
+                return SpmfMiner.extractMaximal(datasetPath, params);
+            default:
+                throw new IllegalArgumentException("Unknown query type: " + queryType);
+        }
+    }
+
+}
