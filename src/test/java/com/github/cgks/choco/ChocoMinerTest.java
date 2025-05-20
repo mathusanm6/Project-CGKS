f7464d31e6197176727a96515319981e06fe4422
jewin.cheng <jewin.cheng@gmail.com>
2025-05-20 17:05:35 +0200

    [fix] fixed test from choco rare itemset query

--- src/test/java/com/github/cgks/choco/ChocoMinerTest.java
+++ src/test/java/com/github/cgks/choco/ChocoMinerTest.java
@@ -467,7 +467,7 @@ class ChocoMinerTest {
         @Test
         @DisplayName("Invalid parameters - Too low support threshold")
         void testExtractRare_TooLowSupport() {
-            Map<String, String> params = Parameters.empty().withParam(MIN_SUPPORT_PARAM, 0.1).getHashMap();
+            Map<String, String> params = Parameters.empty().withParam(MIN_SUPPORT_PARAM, -1).getHashMap();
             assertThrows(ParameterException.class, () -> miner.extractRare(datasetPath, params));
         }
     }
@@ -570,7 +570,7 @@ class ChocoMinerTest {
         @Test
         @DisplayName("Invalid parameters - Too low support threshold")
         void testExtractMinimal_TooLowSupport() {
-            Map<String, String> params = Parameters.empty().withParam(MIN_SUPPORT_PARAM, 0.1).getHashMap();
+            Map<String, String> params = Parameters.empty().withParam(MIN_SUPPORT_PARAM, -1).getHashMap();
             assertThrows(ParameterException.class, () -> miner.extractMinimal(datasetPath, params));
         }
     }
